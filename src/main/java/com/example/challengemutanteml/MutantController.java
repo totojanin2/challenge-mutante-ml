d4054392648431a2db0eb15724e4a5dff06a2f9d
totojanin2 <totojanin2@gmail.com>
2021-01-23 21:21:25 -0300

    cambio a post

--- src/main/java/com/example/challengemutanteml/MutantController.java
+++ src/main/java/com/example/challengemutanteml/MutantController.java
@@ -20,7 +20,7 @@ public class MutantController {
     @Autowired
     private DNARepository dnaRepository;
 
-    @RequestMapping(name = "/mutant", method = RequestMethod.POST)
+    @PostMapping("/mutant")
     public ResponseEntity mutant(@RequestParam String[] dna, @RequestParam int cantLetrasMutante) throws Exception {
         boolean isMutant = Mutant.isMutant(dna, cantLetrasMutante);
 
